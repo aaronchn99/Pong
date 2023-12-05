07a318c3644a16c06461953e76431f82c2dea396
aaronchn99 <36250963+aaronchn99@users.noreply.github.com>
2023-12-05 00:24:20 +0000

    AI algorithms
    - Follow ball
    - Follow but with sticky controls (direction does not change for interval)
    - Follow with corruption (random chance of opposite direction)
    - Sticky follow with corruption

--- Scripts/ControlPaddle.gd
+++ Scripts/ControlPaddle.gd
@@ -3,6 +3,9 @@ extends StaticBody2D
 @export var speed : int
 @export var is_human : bool
 
+enum AI_Type {RANDOM, FOLLOW, FOLLOW_STICKY, FOLLOW_CORRUPT, FOLLOW_CORRUPT_STICKY}
+@export var ai_type : AI_Type
+
 var Rng = RandomNumberGenerator.new()
 
 # Called when the node enters the scene tree for the first time.
@@ -26,13 +29,53 @@ func human_control():
 		return 1
 	return 0
 
-var STARTCD = 20
+@export var UPDATE_INTERVAL = 20
 var countdown = 0
 var dir = 0
-func cpu_control():
+func random_ai():
 	countdown -= 1
 	if countdown <= 0:
 		dir = Rng.randi_range(-1, 1)
-		countdown = STARTCD
+		countdown = UPDATE_INTERVAL
 	return dir
+
+# Follow ball
+func follow_ai():
+	var Ball = get_node("../Ball")
+	return (Ball.position.y - position.y)/abs(Ball.position.y - position.y)
+
+# Follow but with sticky controls (direction does not change for interval)
+func follow_sticky():
+	countdown -= 1
+	if countdown <= 0:
+		dir = follow_ai()
+		countdown = UPDATE_INTERVAL
+	return dir
+
+# Follow with corruption (random chance of going opposite direction)
+@export var CORRUPT_CHANCE = 0.8
+func follow_corrupt():
+	if Rng.randf() > CORRUPT_CHANCE:
+		return follow_ai() * -1
+	return follow_ai()
+
+# Sticky follow with corruption
+func follow_corrupt_sticky():
+	countdown -= 1
+	if countdown <= 0:
+		dir = follow_corrupt()
+		countdown = UPDATE_INTERVAL
+	return dir
+
+func cpu_control():
+	if ai_type == AI_Type.RANDOM:
+		return random_ai()
+	elif ai_type == AI_Type.FOLLOW:
+		return follow_ai()
+	elif ai_type == AI_Type.FOLLOW_STICKY:
+		return follow_sticky()
+	elif ai_type == AI_Type.FOLLOW_CORRUPT:
+		return follow_corrupt()
+	elif ai_type == AI_Type.FOLLOW_CORRUPT_STICKY:
+		return follow_corrupt_sticky()
 	
