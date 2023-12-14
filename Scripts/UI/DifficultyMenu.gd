1e16c070ed3fba8cf0e26d5180d09df53af7b694
aaronchn99 <36250963+aaronchn99@users.noreply.github.com>
2023-12-14 03:57:26 +0000

    Feat: Impossible Mode - Visible with cheat code (Contra)

--- Scripts/UI/DifficultyMenu.gd
+++ Scripts/UI/DifficultyMenu.gd
@@ -17,9 +17,14 @@ var game_scene = preload("res://Game.tscn")
 	'Hard': {
 		'Paddle2' : {
 			'ai_type': 6,
-			'speed': 550
+			'speed': 550,
 		}
-	}
+	},
+	'Impossible': {
+		'Paddle2' : {
+			'ai_type': 5,
+		}
+	},
 }
 
 func _go_back():
@@ -36,3 +41,16 @@ func _play_game(difficulty : String):
 		var obj = $/root/Main/Game.get_node(obj_name)
 		for prop in config[obj_name]:
 			obj[prop] = config[obj_name][prop]
+
+var cheat_code = ['ui_up', 'ui_up', 'ui_down', 'ui_down', 'ui_left', 'ui_right', 'ui_left', 'ui_right',]
+var i = 0
+var timeout = 1
+func _process(delta):
+	if timeout > 0: timeout -= delta
+	else: i = 0
+	
+	if not $ImpossibleButton.visible and Input.is_action_just_released(cheat_code[i]):
+		i += 1
+		timeout = 1
+		if i >= len(cheat_code):
+			$ImpossibleButton.visible = true
